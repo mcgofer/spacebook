2b0fc3b759f41fdbad25221f606d213b3168ec88
MC <taliaferromc@gmail.com>
2018-01-02 13:37:12 +0200

    functioning remove button

--- main.js
+++ main.js
@@ -1,27 +1,25 @@
 var posts = [];
+var idNumber = 0;
 
-function addPost(text, id) {
-    var post = { "text": text, "id": id };
+$('.add-post').click(function () {
+    var post = { text: $("#post-name").val(), id: idNumber++, comments: [] };
     posts.push(post);
-}
+    renderPosts();
+});
 
 function renderPosts() {
-    $(".posts").find("p").remove();
+    $(".posts").empty();
     for (var i = 0; i < posts.length; i++) {
         $('.posts').append('<p class="post" data-id=' + posts[i].id + '>' + '<button type="button" class="remove">REMOVE</button> ' + posts[i].text + '</p>');
     }
 }
 
-var idNumber = 0;
-$('.add-post').click(function () {
-    addPost($("#post-name").val(), idNumber++);
-    renderPosts();
-});
 
 $('.posts').on('click', '.remove', function () {
     var removeId = $(this).closest('p').data().id;
-    // alert(removeId);
-    posts.splice(removeId, 1);
+    for (var i = 0; i < posts.length; i++) {
+        if (removeId === posts[i].id)
+            posts.splice(i, 1);
+    }
     renderPosts();
 });
-
